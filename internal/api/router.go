244cd8e96a08f11a8bc219d4ec8e22cdcf0bbad3
Gizmo <cunyu@liuli.lol>
2023-02-20 05:03:43 +0800

    fix: naming error

--- internal/api/router.go
+++ internal/api/router.go
@@ -5,7 +5,7 @@ import (
 	middleware2 "github.com/gizmo-ds/misstodon/internal/api/middleware"
 	"github.com/gizmo-ds/misstodon/internal/api/nodeinfo"
 	"github.com/gizmo-ds/misstodon/internal/api/oauth"
-	v12 "github.com/gizmo-ds/misstodon/internal/api/v1"
+	v1 "github.com/gizmo-ds/misstodon/internal/api/v1"
 	"github.com/gizmo-ds/misstodon/internal/api/wellknown"
 	"github.com/gizmo-ds/misstodon/internal/global"
 	"github.com/labstack/echo/v4"
@@ -27,11 +27,11 @@ func Router(e *echo.Echo) {
 		nodeinfo.Router(group)
 		oauth.Router(group)
 		v1Api := group.Group("/api/v1", middleware2.CORS())
-		group.GET("/static/missing.png", v12.MissingImageHandler)
-		v12.InstanceRouter(v1Api)
-		v12.AccountsRouter(v1Api)
-		v12.ApplicationRouter(v1Api)
-		v12.StatusesRouter(v1Api)
-		v12.StreamingRouter(v1Api)
+		group.GET("/static/missing.png", v1.MissingImageHandler)
+		v1.InstanceRouter(v1Api)
+		v1.AccountsRouter(v1Api)
+		v1.ApplicationRouter(v1Api)
+		v1.StatusesRouter(v1Api)
+		v1.StreamingRouter(v1Api)
 	}
 }
